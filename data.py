c048d9cd3c41bfe0c533a4749a6e9cb831ee1a3d
belbrun <brunobelic@hotmail.com>
2021-05-06 21:26:57 +0200

    Implement record spliting, overall data loading

--- data.py
+++ data.py
@@ -1,29 +1,50 @@
 import os
 import wfdb
+import matplotlib.pyplot as plt
+import numpy as np
 
-
+files = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114,
+         115, 116, 117, 118, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205,
+         207, 208, 209, 210, 212, 213, 214, 215, 217, 219, 220, 221, 222, 223,
+         228, 230, 231, 232, 233, 234]
 def load_example(path):
     signal = wfdb.rdsamp(path, sampto=None)
     annotation = wfdb.rdann(path, 'atr', sampto=None)
-    return record, annotation
+    return signal, annotation
 
-def split_record(signal, samples):
+def split_record(signal, sample):
     beats = []
-    pos = samples[0]
-    samples = samples[1:]
-    for i in range(len(samples)):
-        beat = signal[pos: sample[]]
+    pos = sample[0]
+    sample = sample[1:]
+    for i in range(len(sample)-1):
+        diff = (sample[i+1]-sample[i])//5
+        beats.append(signal[pos: sample[i+1]-diff])
+        pos = sample[i+1] - diff
+    beats.append(signal[pos:])
+    return beats
 
+def get_mitbih():
+    dataset_path = os.path.join('dataset', 'mit-bih'    )
+    beats, annotations = [], []
+    for i in files:
+        s, a = load_example(os.path.join(dataset_path, str(i)))
+        beats.extend(split_record(s[0], a.sample))
+        annotations.extend(a.symbol[1:])
+    return beats, annotations
 
-def plot(path, sampto):
+def plot_record(path, sampto=None):
     record = wfdb.rdrecord(path, sampto=sampto)
     annotation = wfdb.rdann(path, 'atr', sampto=sampto)
     wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
                    time_units='seconds', title='MIT-BIH Record 100',
                    figsize=(10,4), ecg_grids='all')
 
+def plot(beat):
+    t = np.arange(beat.shape[0])
+    plt.plot(t, beat[:, 0])
+    plt.show()
+
+
 if __name__ == '__main__':
-    plot('dataset/mit-bih/109', 3000)
-    r, a = load_example('dataset/mit-bih/109')
-    print(a.symbol[:100])
-    print(sum(a.sample[2:]-a.sample[1:-1])/(len(a.sample)-1))
+    X, y = get_mitbih()
+    print(len(X), len(y))
